06f187a9db1e426a9c5b2684697f126d32361a15
Ultra Zacky <ultrazacky@gmail.com>
2022-08-21 14:38:39 -0700

    add styles, icons and layout for Header *RIGHT*

--- components/Header.js
+++ components/Header.js
@@ -30,7 +30,7 @@ function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
           />
@@ -40,7 +40,7 @@ function Header() {
       {/* TOP-CENTER */}
       <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md:space-x-2">
-          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon active Icon={HomeIcon} />
           <HeaderIcon Icon={FlagIcon} />
           <HeaderIcon Icon={PlayIcon} />
           <HeaderIcon Icon={ShoppingCartIcon} />
@@ -49,8 +49,14 @@ function Header() {
       </div>
 
       {/* TOP-RIGHT */}
-      <div>
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* PROFILE PIC */}
 
+        <p className="whitespace-nowrap font-semibold pr-3">{/* USERNAME */}</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
       </div>
     </header>
   )
