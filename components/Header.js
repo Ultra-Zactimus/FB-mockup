e20bfcf54171577f42da36bd1468f3c77bcf7fd2
Ultra Zacky <ultrazacky@gmail.com>
2022-08-21 14:24:40 -0700

    add styles to Header *CENTER* section and header tag

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,7 @@ import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
-    <header>
+    <header className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* TOP-LEFT */}
       <div className="flex items-center">
         <Image
@@ -29,13 +29,17 @@ function Header() {
         />
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
-          <input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search Facebook" />
+          <input
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
 
       {/* TOP-CENTER */}
       <div className="flex justify-center flex-grow">
-        <div className="">
+        <div className="flex space-x-6 md:space-x-2">
           <HeaderIcon Icon={HomeIcon} />
           <HeaderIcon Icon={FlagIcon} />
           <HeaderIcon Icon={PlayIcon} />
