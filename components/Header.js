e070b0fca39e73fbe335d4788b8894ee6dba9515
Ultra Zacky <ultrazacky@gmail.com>
2022-08-21 12:44:12 -0700

    add layout for header and Image for left side

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,25 @@
+import Image from "next/image"
 
 
 function Header() {
   return (
     <header>
+      <div>
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+      </div>
 
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
     </header>
   )
 }
